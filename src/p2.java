95572ee978918dfbd97e1f23494f24fd6832973d
Niyati Sinha <niyati.sinha2999@gmail.com>
2021-01-30 23:13:34 +0530

    initial commit

---
+++ src/p2.java
@@ -0,0 +1,13 @@
+public class p2 {
+    public static void main(String[] args) {
+        int age=10;
+        char section='D';
+        boolean isAdult=false;
+        String name="Joe";
+
+        System.out.println(age);
+        System.out.println(section);
+        System.out.println(isAdult);
+        System.out.println(name);
+    }
+}
