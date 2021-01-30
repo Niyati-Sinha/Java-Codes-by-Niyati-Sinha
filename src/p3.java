ecd79e74f8862f942358325ea585629b43b31c45
Niyati Sinha <niyati.sinha2999@gmail.com>
2021-01-30 23:26:56 +0530

    initial commit

--- src/p3.java
+++ src/p3.java
@@ -1,11 +1,11 @@
 public class p3 {
     public static void main(String[] args) {
-        int age=20;
+        //int age=20;
         boolean isAdult=true;
         String name="Niyati Sinha";
         char studentFirstNameInitial=name.charAt(0);
 
-        System.out.println(age);
+        //System.out.println(age);
         System.out.println(studentFirstNameInitial);
         System.out.println(isAdult);
         System.out.println(name);
