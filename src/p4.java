c9decec8ac59f134902d664a2d41fad9a230cb45
Niyati Sinha <niyati.sinha2999@gmail.com>
2021-01-31 00:25:09 +0530

    initial commit

---
+++ src/p4.java
@@ -0,0 +1,8 @@
+//String Concatenation
+public class p4 {
+    public static void main(String[] args) {
+    String firstName = "Niyati";
+    String lastName = "Sinha";
+    System.out.println(firstName+" "+lastName);
+    }
+}
---
+++ src/p5.java
@@ -0,0 +1,12 @@
+//taking user inpurt and updating variables
+import java.util.Scanner;
+
+public class p5 {
+    public static void main(String[] args) {
+        double cgpa=9.70;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Üpdate cgpa :");
+        cgpa= sc.nextDouble();
+        System.out.println(cgpa);
+    }
+}
